8aa758f633880ffbba727a7e73c1f0284100094b
Tyler Flint <tylerflint@gmail.com>
2016-01-26 14:05:12 -0700

    Create a console helper for replicating test environment

---
+++ test/console.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Launches the test environment within a docker container
+# and drops into a console
+
+test_dir="$(dirname $(readlink -f $BASH_SOURCE))"
+engine_dir="$(dirname ${test_dir})"
+
+# source the env helper
+. $test_dir/util/env.sh
+
+docker run \
+  -it \
+  -u=gonano \
+  --privileged=true \
+  --workdir=/test \
+  -e "PATH=$(path)" \
+  --volume=${test_dir}/:/test \
+  --volume=${engine_dir}/:/engine \
+  nanobox/build \
+  /bin/bash
